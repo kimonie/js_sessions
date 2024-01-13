4f1167e24cdba05f3a0e8aa4e4e335e73fb556fb
kimonie <seunkikiowo@gmail.com>
2024-01-13 10:19:04 +0100

    quote generator completed

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+
 // SHOW NEW QUOTE
 function newQuote() {
-
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     // If Author field is null
@@ -26,25 +40,37 @@ function newQuote() {
     }
 
     quoteText.textContent = quote.text;
-
+    complete();
 }
 
 // GET QUOTES FROM API
 async function getQuote() {
-
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     
     try {
-        
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
 
     } catch (error) {
         // Catch Error
+        console.log('error: ', error)
     }
 
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+    window.open(twitterUrl, '_blank');
+}
+
+//  Button Eventlisteners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 
 // On Load
 getQuote();
